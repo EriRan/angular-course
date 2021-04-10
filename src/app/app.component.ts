16adc19d92ed079d3e249fedee5666b265b6e7de
Erik <erik.rantanen@hotmail.fi>
2021-04-10 14:36:16 +0300

    Replace the main page with a form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 
 //Interesting! This is where some variables that are used in this component
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', //This is the element in index.html
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'gay-course';
+  name = 'gay-course';
 }
