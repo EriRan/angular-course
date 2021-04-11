02abc4dccc11fc1478f7fe93ae918eaa41947951
Erik <erik.rantanen@hotmail.fi>
2021-04-11 19:15:01 +0300

    Remove button disable logic
    This was removed at the start of a video

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -10,16 +10,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css'],
 })
 export class ServersComponent implements OnInit {
-  allowNewServer = false;
   serverCreationStatus = 'No server was created!';
   serverName: string = '';
   serverCreated = false;
 
-  constructor() {
-    setTimeout(() => {
-      this.allowNewServer = true;
-    }, 2000);
-  }
+  constructor() {}
 
   ngOnInit(): void {}
 
