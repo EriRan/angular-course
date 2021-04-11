2c6342c522fb8bafc038dde7575886a399369b13
Erik <erik.rantanen@hotmail.fi>
2021-04-11 14:07:57 +0300

    Add warning and success components
    The ones from the first assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root', //This is the element in index.html
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.css'], //This can also be just style if you want to add inline style. I think inline styles are gay
 })
 export class AppComponent {}
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
-  templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+  //Selector defines a way of how the component is added to the html. It can be added
+  //to a element by a class name, as an attribute or just with its own element tag.
+  //Selecting by id is not supported
+  //selector: '[app-servers]',
+  selector: '.app-servers',
+  template: `<app-server></app-server><app-server></app-server>`,
+  styleUrls: ['./servers.component.css'],
 })
 export class ServersComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
