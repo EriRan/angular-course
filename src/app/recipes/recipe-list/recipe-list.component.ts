7306ab84c356ca10e47131592f26b9f33f775aa8
EriRan <erik.rantanen@hotmail.fi>
2021-04-12 14:56:58 +0300

    Display placeholder recipes

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Recipe} from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,7 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
-  constructor() { }
+  recipes: Array<Recipe> = [
+    new Recipe('Cool Bread', 'Really cool', 'https://en.wikipedia.org/wiki/Sourdough#/media/File:Home_made_sour_dough_bread.jpg')
+  ];
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
