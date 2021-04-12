73fbd169465e1797f7a2d77ccfe417745d73297d
EriRan <erik.rantanen@hotmail.fi>
2021-04-12 12:00:58 +0300

    Reset code
    Code reset for the start of the course project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,12 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
-import { SuccessAlertComponent } from './success-alert/success-alert.component';
-import { WarningAlertComponent } from './warning-alert/warning-alert.component';
-import { LogEntryComponent } from './log-entry/log-entry.component';
 
 //Some kind of definition file for the component?
 //NgModule here is also a decorator
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent,
-    ServersComponent,
-    WarningAlertComponent,
-    SuccessAlertComponent,
-    LogEntryComponent,
   ],
   imports: [BrowserModule, FormsModule],
   providers: [],
