bea2d9d3eb9391cbb692ff3ccbcaf57c442d30ac
EriRan <erik.rantanen@hotmail.fi>
2021-04-12 12:31:33 +0300

    Add skeleton components
    This is for the first course project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
-//Some kind of definition file for the component?
-//NgModule here is also a decorator
+// Some kind of definition file for the component?
+// NgModule here is also a decorator
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent,
   ],
   imports: [BrowserModule, FormsModule],
   providers: [],
-  //Which component should be aware of at the app startup
+  // Which component should be aware of at the app startup
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+}
