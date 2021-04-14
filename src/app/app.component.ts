7913a2a9bfc6f2795b689d3d0028abee08ee1cf5
EriRan <erik.rantanen@hotmail.fi>
2021-04-14 11:46:11 +0300

    Add currently displayed feature toggle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
 
-//Interesting! This is where some variables that are used in this component
+// Interesting! This is where some variables that are used in this component
 @Component({
-  selector: 'app-root', //This is the element in index.html
+  selector: 'app-root', // This is the element in index.html
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'], //This can also be just style if you want to add inline style. I think inline styles are gay
+  styleUrls: ['./app.component.css'], // This can also be just style if you want to add inline style. I think inline styles are gay
 })
-export class AppComponent {}
+export class AppComponent {
+  loadedFeature = 'recipe';
+
+  onNavigate(feature: string): void {
+    this.loadedFeature = feature;
+  }
+}
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, EventEmitter, Output} from '@angular/core';
 
 @Component(
   {
@@ -9,4 +9,9 @@ import {Component} from '@angular/core';
 )
 export class HeaderComponent {
   public isMenuCollapsed = true;
+  @Output() featureSelected = new EventEmitter<string>();
+
+  onSelect(feature: string): void {
+    this.featureSelected.emit(feature);
+  }
 }
