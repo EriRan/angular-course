4d2ed4373d8f8aea232e0b3a4aa6d31dd05bae6c
EriRan <erik.rantanen@hotmail.fi>
2021-04-24 16:52:45 +0300

    Add more work on the directives
    Use input annotations and host binding

--- side-project/src/app/better-highlight/better-highlight.directive.ts
+++ side-project/src/app/better-highlight/better-highlight.directive.ts
@@ -1,25 +1,34 @@
-import { Directive, ElementRef, HostBinding, HostListener, OnInit, Renderer2 } from "@angular/core";
+import {
+  Directive,
+  ElementRef,
+  HostBinding,
+  HostListener,
+  Input,
+  OnInit,
+  Renderer2,
+} from "@angular/core";
 
 @Directive({
   selector: "[appBetterHighlight]",
 })
 export class BetterHighlightDirective implements OnInit {
-
-  @HostBinding('style.backgroundColor') backgroundColor: string = 'transparent';
+  @Input() defaultColor: string = "yellow";
+  @Input('appBetterHighlight') highlightColor: string = "blue";
+  @HostBinding("style.backgroundColor") backgroundColor: string;
 
   constructor(private elRef: ElementRef, private renderer: Renderer2) {}
 
   ngOnInit() {
-
+    this.backgroundColor = this.defaultColor;
   }
 
-  @HostListener('mouseenter')
+  @HostListener("mouseenter")
   mouseover(eventData: Event) {
-    this.backgroundColor = 'blue';
+    this.backgroundColor = this.highlightColor;
   }
 
-  @HostListener('mouseleave')
+  @HostListener("mouseleave")
   mouseleave(eventData: Event) {
-    this.backgroundColor = 'transparent';
+    this.backgroundColor = this.defaultColor;
   }
 }
