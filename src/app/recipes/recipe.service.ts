03f8e9841f7e72a233c8de26e11912cce05aaa6c
EriRan <erik.rantanen@hotmail.fi>
2021-04-25 18:59:05 +0300

    Add ingredients to recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,5 @@
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 import { Recipe } from './recipe.model';
 
 export class RecipeService {
@@ -8,12 +9,14 @@ export class RecipeService {
     new Recipe(
       'Cool Bread',
       'Really cool',
-      'https://en.wikipedia.org/wiki/Sourdough#/media/File:Home_made_sour_dough_bread.jpg'
+      'https://en.wikipedia.org/wiki/Sourdough#/media/File:Home_made_sour_dough_bread.jpg',
+      [new Ingredient('Flour', 34), new Ingredient('Salt', 6)]
     ),
     new Recipe(
       'Mango Lassi',
       'Sweet like honey',
-      'https://en.wikipedia.org/wiki/Sourdough#/media/File:Home_made_sour_dough_bread.jpg'
+      'https://en.wikipedia.org/wiki/Sourdough#/media/File:Home_made_sour_dough_bread.jpg',
+      [new Ingredient('Mango', 2)]
     ),
   ];
 
