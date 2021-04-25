414bff9a40d5ed81329f97ccac0665627e3d4eff
EriRan <erik.rantanen@hotmail.fi>
2021-04-25 18:31:11 +0300

    Handle recipe selection with RecipeService

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,13 +1,24 @@
-import { Recipe } from "./recipe.model";
+import { EventEmitter } from '@angular/core';
+import { Recipe } from './recipe.model';
 
 export class RecipeService {
-    private recipes: Array<Recipe> = [
-        new Recipe('Cool Bread', 'Really cool', 'https://en.wikipedia.org/wiki/Sourdough#/media/File:Home_made_sour_dough_bread.jpg'),
-        new Recipe('Mango Lassi', 'Sweet like honey', 'https://en.wikipedia.org/wiki/Sourdough#/media/File:Home_made_sour_dough_bread.jpg')
-      ];
+  recipeSelected = new EventEmitter<Recipe>();
 
-      getRecipes() {
-          //Returns a new array that contains exact copy
-          return this.recipes.slice();
-      }
+  private recipes: Array<Recipe> = [
+    new Recipe(
+      'Cool Bread',
+      'Really cool',
+      'https://en.wikipedia.org/wiki/Sourdough#/media/File:Home_made_sour_dough_bread.jpg'
+    ),
+    new Recipe(
+      'Mango Lassi',
+      'Sweet like honey',
+      'https://en.wikipedia.org/wiki/Sourdough#/media/File:Home_made_sour_dough_bread.jpg'
+    ),
+  ];
+
+  getRecipes() {
+    //Returns a new array that contains exact copy
+    return this.recipes.slice();
+  }
 }
