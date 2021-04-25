023577a312123824000f9f64c336d347677b95c7
EriRan <erik.rantanen@hotmail.fi>
2021-04-25 15:24:15 +0300

    Make loggingService be used in accountService
    Done with Angular dependency injection

--- side-project/src/app/account/account.component.ts
+++ side-project/src/app/account/account.component.ts
@@ -1,21 +1,18 @@
-import { Component, Input } from '@angular/core';
-import { AccountsService } from '../account.service';
-import { LoggingService } from '../logging.service';
+import { Component, Input } from "@angular/core";
+import { AccountsService } from "../account.service";
 
 @Component({
-  selector: 'app-account',
-  templateUrl: './account.component.html',
-  styleUrls: ['./account.component.css'],
-  providers: [LoggingService]
+  selector: "app-account",
+  templateUrl: "./account.component.html",
+  styleUrls: ["./account.component.css"],
 })
 export class AccountComponent {
-  @Input() account: {name: string, status: string};
+  @Input() account: { name: string; status: string };
   @Input() id: number;
 
-  constructor(private loggingService: LoggingService, private accountsService: AccountsService) {}
+  constructor(private accountsService: AccountsService) {}
 
   onSetTo(status: string) {
     this.accountsService.updateStatus(this.id, status);
-    this.loggingService.logStatusChange(status);
   }
 }
--- side-project/src/app/new-account/new-account.component.ts
+++ side-project/src/app/new-account/new-account.component.ts
@@ -1,22 +1,16 @@
 import { Component } from "@angular/core";
 import { AccountsService } from "../account.service";
-import { LoggingService } from "../logging.service";
 
 @Component({
   selector: "app-new-account",
   templateUrl: "./new-account.component.html",
   styleUrls: ["./new-account.component.css"],
-  providers: [LoggingService],
 })
 export class NewAccountComponent {
   //A Typescript Type has to be set in the injection!!!
-  constructor(
-    private loggingService: LoggingService,
-    private accountsService: AccountsService
-  ) {}
+  constructor(private accountsService: AccountsService) {}
 
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountsService.addAccount(accountName, accountStatus);
-    this.loggingService.logStatusChange(accountStatus);
   }
 }
