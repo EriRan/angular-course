ab302fd73b0f79a07d52b528c905b6b4a60b79fd
EriRan <erik.rantanen@hotmail.fi>
2021-05-01 15:08:59 +0300

    Use observable params subscription
    This way the parameters from the URL are always used

--- side-project/src/app/users/user/user.component.ts
+++ side-project/src/app/users/user/user.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-import { ActivatedRoute } from "@angular/router";
+import { ActivatedRoute, Params } from "@angular/router";
 
 @Component({
   selector: "app-user",
@@ -16,5 +16,15 @@ export class UserComponent implements OnInit {
       id: this.route.snapshot.params["id"],
       name: this.route.snapshot.params["name"],
     };
+    //params vs. snapshot
+    //Params is an observable. We can observe it by subscribing to it
+    //Oh wow this is the one I know
+    //Whenever new data is sent (parameters change), we execute the function again
+    this.route.params.subscribe(
+      (params: Params) => {
+        this.user.id = params['id'],
+        this.user.name = params['name']
+      }
+    )
   }
 }
