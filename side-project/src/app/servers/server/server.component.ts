f45fc6d83abdea44e6af5f34dc270e11c0d6b146
EriRan <erik.rantanen@hotmail.fi>
2021-05-01 17:43:04 +0300

    Add edit server button

--- side-project/src/app/servers/server/server.component.ts
+++ side-project/src/app/servers/server/server.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-import { ActivatedRoute, Params } from "@angular/router";
+import { ActivatedRoute, Params, Router } from "@angular/router";
 
 import { ServersService } from "../servers.service";
 
@@ -13,7 +13,8 @@ export class ServerComponent implements OnInit {
 
   constructor(
     private serversService: ServersService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private router: Router
   ) {}
 
   ngOnInit() {
@@ -23,5 +24,10 @@ export class ServerComponent implements OnInit {
     this.route.params.subscribe((params: Params) => {
       this.server = this.serversService.getServer(+params["id"]);
     });
+  }
+
+  onEdit() {
+    //We are already have the servers and the id in the URL, so we can just append edit to the end
+    this.router.navigate(["edit"], { relativeTo: this.route });
   }
 }
