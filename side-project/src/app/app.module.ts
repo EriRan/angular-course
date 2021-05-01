dc7dec339d6cba608e378fecf668db0ce130eba8
EriRan <erik.rantanen@hotmail.fi>
2021-05-01 15:58:42 +0300

    Add nested routing for users and servers

--- side-project/src/app/app.module.ts
+++ side-project/src/app/app.module.ts
@@ -16,11 +16,19 @@ const appRoutes: Routes = [
   //This is what gets rendered to the URL
   //Don't add slash here
   { path: "", component: HomeComponent },
-  { path: "users", component: UsersComponent },
-  { path: "users/:id/:name", component: UserComponent },
-  { path: "servers", component: ServersComponent },
-  { path: "servers/:id", component: ServerComponent },
-  { path: "servers/:id/edit", component: EditServerComponent },
+  {
+    path: "users",
+    component: UsersComponent,
+    children: [{ path: ":id/:name", component: UserComponent }],
+  },
+  {
+    path: "servers",
+    component: ServersComponent,
+    children: [
+      { path: ":id", component: ServerComponent },
+      { path: ":id/edit", component: EditServerComponent },
+    ],
+  },
 ];
 
 @NgModule({
