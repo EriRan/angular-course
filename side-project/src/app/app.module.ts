e1c69cbf8a51f7998056181665479d4f0c02f75a
EriRan <erik.rantanen@hotmail.fi>
2021-05-01 18:01:00 +0300

    Add page not found component
    Is ran as the last route so that all non available URLs end up here

--- side-project/src/app/app.module.ts
+++ side-project/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { EditServerComponent } from "./servers/edit-server/edit-server.component
 import { ServerComponent } from "./servers/server/server.component";
 import { ServersService } from "./servers/servers.service";
 import { RouterModule, Routes } from "@angular/router";
+import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
 
 const appRoutes: Routes = [
   //This is what gets rendered to the URL
@@ -29,6 +30,14 @@ const appRoutes: Routes = [
       { path: ":id/edit", component: EditServerComponent },
     ],
   },
+  {
+    path: "not-found",
+    component: PageNotFoundComponent,
+  },
+  {
+    path: "**",
+    redirectTo: "/not-found",
+  },
 ];
 
 @NgModule({
@@ -40,6 +49,7 @@ const appRoutes: Routes = [
     UserComponent,
     EditServerComponent,
     ServerComponent,
+    PageNotFoundComponent,
   ],
   imports: [BrowserModule, FormsModule, RouterModule.forRoot(appRoutes)],
   providers: [ServersService],
