8a7f8a8b6175cc7470461e10dd3f6af06f55aef9
EriRan <erik.rantanen@hotmail.fi>
2021-05-01 14:07:58 +0300

    Add simple programmatic routing to home component

--- side-project/src/app/home/home.component.ts
+++ side-project/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  onLoadServers() {
+    // This could have really complex backend calls, then it renders the servers!!!!
+    //This is how you trigger the router programmatically
+    this.router.navigate(['/servers']);
   }
 
 }
