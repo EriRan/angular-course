5d8c9181775c0c5a769a6bc48a86fceaee2f0e3d
EriRan <erik.rantanen@hotmail.fi>
2021-05-01 13:13:35 +0300

    Switch to routing side project

--- side-project/src/app/app.component.ts
+++ side-project/src/app/app.component.ts
@@ -1,19 +1,9 @@
-import { Component, OnInit } from "@angular/core";
-import { UsersService } from "./users.service";
+import { Component } from '@angular/core';
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"],
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  activeUsers: Array<string>;
-  inactiveUsers: Array<string>;
-
-  constructor(private usersService: UsersService) {}
-
-  ngOnInit(): void {
-    this.activeUsers = this.usersService.activeUsers;
-    this.inactiveUsers = this.usersService.inactiveUsers;
-  }
+export class AppComponent {
 }
