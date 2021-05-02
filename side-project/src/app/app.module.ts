602db31937fe590337c3814fac279077c65c1cec
EriRan <erik.rantanen@hotmail.fi>
2021-05-02 13:14:07 +0300

    Extract routing to a separate file

--- side-project/src/app/app.module.ts
+++ side-project/src/app/app.module.ts
@@ -10,35 +10,8 @@ import { UserComponent } from "./users/user/user.component";
 import { EditServerComponent } from "./servers/edit-server/edit-server.component";
 import { ServerComponent } from "./servers/server/server.component";
 import { ServersService } from "./servers/servers.service";
-import { RouterModule, Routes } from "@angular/router";
 import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
-
-const appRoutes: Routes = [
-  //This is what gets rendered to the URL
-  //Don't add slash here
-  { path: "", component: HomeComponent },
-  {
-    path: "users",
-    component: UsersComponent,
-    children: [{ path: ":id/:name", component: UserComponent }],
-  },
-  {
-    path: "servers",
-    component: ServersComponent,
-    children: [
-      { path: ":id", component: ServerComponent },
-      { path: ":id/edit", component: EditServerComponent },
-    ],
-  },
-  {
-    path: "not-found",
-    component: PageNotFoundComponent,
-  },
-  {
-    path: "**",
-    redirectTo: "/not-found",
-  },
-];
+import { AppRoutingModule } from "./app-routing.module";
 
 @NgModule({
   declarations: [
@@ -51,7 +24,7 @@ const appRoutes: Routes = [
     ServerComponent,
     PageNotFoundComponent,
   ],
-  imports: [BrowserModule, FormsModule, RouterModule.forRoot(appRoutes)],
+  imports: [BrowserModule, FormsModule, AppRoutingModule],
   providers: [ServersService],
   bootstrap: [AppComponent],
 })
