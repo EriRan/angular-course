bf1280b58cf112cf1ac7fd6ae2b63a0a2a4b5995
EriRan <erik.rantanen@hotmail.fi>
2021-05-02 13:47:42 +0300

    Add console logs
    To give some indication whether user is logged in or not

--- side-project/src/app/auth.service.ts
+++ side-project/src/app/auth.service.ts
@@ -2,20 +2,20 @@ export class AuthService {
   loggedIn = false;
 
   isAuthenticated() {
-      return new Promise(
-        (resolve, reject) => {
-            setTimeout(() => {
-                resolve(this.loggedIn)
-            }, 800)
-        }
-      );
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(this.loggedIn);
+      }, 800);
+    });
   }
 
   login() {
+    console.log("Logged in");
     this.loggedIn = true;
   }
 
   logout() {
+    console.log("Logged out");
     this.loggedIn = false;
   }
 }
