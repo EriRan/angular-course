c867bb673cd60632f9668205237d29ae4e52b96c
EriRan <erik.rantanen@hotmail.fi>
2021-05-02 17:52:18 +0300

    Remove unused navigation method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'], // This can also be just style if you want to add inline style. I think inline styles are gay
 })
-export class AppComponent {
-  loadedFeature = 'recipe';
-
-  onNavigate(feature: string): void {
-    this.loadedFeature = feature;
-  }
-}
+export class AppComponent {}
