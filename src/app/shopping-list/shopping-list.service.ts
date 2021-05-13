f34caf50b4c00c7e35ae65cb85e9135d70529b94
EriRan <erik.rantanen@hotmail.fi>
2021-05-13 17:57:14 +0300

    Replace EventEmitter with Subject

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,9 +1,8 @@
-import { EventEmitter } from '@angular/core';
+import { Subject } from 'side-project/node_modules/rxjs';
 import { Ingredient } from '../shared/ingredient.model';
 
 export class ShoppingListService {
-
-ingredientsChanged = new EventEmitter<Ingredient[]>();
+  ingredientsChanged = new Subject<Ingredient[]>();
 
   private ingredients: Array<Ingredient> = [
     new Ingredient('Ginger', 123),
@@ -16,12 +15,12 @@ ingredientsChanged = new EventEmitter<Ingredient[]>();
 
   addIngredient(ingredient: Ingredient) {
     this.ingredients.push(ingredient);
-    this.ingredientsChanged.emit(this.ingredients.slice());
+    this.ingredientsChanged.next(this.ingredients.slice());
   }
 
   addIngredients(ingredients: Ingredient[]) {
     //Dot dot dot is a spread operator. Turns an array into multiple pararameters
     this.ingredients.push(...ingredients);
-    this.ingredientsChanged.emit(this.ingredients.slice());
+    this.ingredientsChanged.next(this.ingredients.slice());
   }
 }
