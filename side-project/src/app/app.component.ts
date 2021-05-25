ff803e1210ef3629de321e22f44ae6fd061091d4
EriRan <erik.rantanen@hotmail.fi>
2021-05-25 17:23:42 +0300

    Switch side project to forms

--- side-project/src/app/app.component.ts
+++ side-project/src/app/app.component.ts
@@ -1,27 +1,12 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
-import { UserService } from './user.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit, OnDestroy {
-
-  userActivated = false;
-  private subscription: Subscription;
-
-  constructor(private userService: UserService) {}
-
-  ngOnInit() {
-    this.subscription = this.userService.activatedEmitter.subscribe(didActivate => {
-      this.userActivated = didActivate;
-    })
+export class AppComponent {
+  suggestUserName() {
+    const suggestedName = 'Superuser';
   }
-
-  ngOnDestroy(): void {
-    this.subscription.unsubscribe();
-  }
-  
 }
