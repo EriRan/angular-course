68f6cbde38f2bd19a593fad2dc1dec1bd22d65e8
EriRan <erik.rantanen@hotmail.fi>
2021-05-27 20:21:51 +0300

    Switch to Template Driven form assignment

--- side-project/src/app/app.component.ts
+++ side-project/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component, ViewChild } from "@angular/core";
+import { ViewChild } from "@angular/core";
+import { Component } from "@angular/core";
 import { NgForm } from "@angular/forms";
 
 @Component({
@@ -8,32 +9,21 @@ import { NgForm } from "@angular/forms";
 })
 export class AppComponent {
   @ViewChild("f") signupForm: NgForm;
-  defaultQuestion = "pet";
-  answer = "";
-  genders = ["Male", "Female"];
-  user = {
-    username: "",
-    email: "",
-    secretQuestion: "",
-    answer: "",
-    gender: "",
-  };
+  subscription = "advanced";
   submitted = false;
-
-  suggestUserName() {
-    const suggestedName = "Superuser";
-    this.signupForm.form.patchValue({ userData: { username: suggestedName } });
-    console.log(this.user);
-  }
+  submittedResult = {
+    email: "",
+    subscription: "",
+    password: "",
+  };
 
   onSubmit() {
     this.submitted = true;
-    this.user.username = this.signupForm.value.userData.username;
-    this.user.email = this.signupForm.value.userData.email;
-    this.user.secretQuestion = this.signupForm.value.secret;
-    this.user.answer = this.signupForm.value.questionAnswer;
-    this.user.gender = this.signupForm.value.gender;
-
+    this.submittedResult.email = this.signupForm.form.value.userData.email;
+    this.submittedResult.subscription =
+      this.signupForm.form.value.userData.subscription;
+    this.submittedResult.password =
+      this.signupForm.form.value.userData.password;
     this.signupForm.resetForm();
   }
 }
