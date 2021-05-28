171b50e075818bbf81c62cf4178099b9263718c6
EriRan <erik.rantanen@hotmail.fi>
2021-05-28 18:27:42 +0300

    Switch to reactive forms section

--- side-project/src/app/app.component.ts
+++ side-project/src/app/app.component.ts
@@ -1,29 +1,10 @@
-import { ViewChild } from "@angular/core";
-import { Component } from "@angular/core";
-import { NgForm } from "@angular/forms";
+import { Component } from '@angular/core';
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"],
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  @ViewChild("f") signupForm: NgForm;
-  subscription = "advanced";
-  submitted = false;
-  submittedResult = {
-    email: "",
-    subscription: "",
-    password: "",
-  };
-
-  onSubmit() {
-    this.submitted = true;
-    this.submittedResult.email = this.signupForm.form.value.userData.email;
-    this.submittedResult.subscription =
-      this.signupForm.form.value.userData.subscription;
-    this.submittedResult.password =
-      this.signupForm.form.value.userData.password;
-    this.signupForm.resetForm();
-  }
+  genders = ['male', 'female'];
 }
