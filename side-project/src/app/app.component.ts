d1be65cd0001007b3a18734306cc008b44ed6591
EriRan <erik.rantanen@hotmail.fi>
2021-05-29 14:30:53 +0300

    Add asynchronous validator for emails

--- side-project/src/app/app.component.ts
+++ side-project/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { OnInit } from "@angular/core";
 import { Component } from "@angular/core";
 import { FormArray } from "@angular/forms";
 import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { Observable } from "rxjs";
 
 @Component({
   selector: "app-root",
@@ -17,8 +18,15 @@ export class AppComponent implements OnInit {
   ngOnInit(): void {
     this.signupForm = new FormGroup({
       userData: new FormGroup({
-        username: new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
-        email: new FormControl(null, [Validators.required, Validators.email]),
+        username: new FormControl(null, [
+          Validators.required,
+          this.forbiddenNames.bind(this),
+        ]),
+        email: new FormControl(
+          null,
+          [Validators.required, Validators.email],
+          this.forbiddenEmails
+        ),
       }),
       gender: new FormControl("male"),
       hobbies: new FormArray([]),
@@ -44,9 +52,22 @@ export class AppComponent implements OnInit {
    */
   forbiddenNames(control: FormControl): { [s: string]: boolean } {
     if (this.forbiddenUsernames.indexOf(control.value) !== -1) {
-      return {'nameIsForbidden': true};
+      return { nameIsForbidden: true };
     }
     //Validation is succesful. Null must be always returned in this case
     return null;
+  }
+
+  forbiddenEmails(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === "test@test.com") {
+          resolve({ emailIsForbidden: true });
+        } else {
+          resolve(null);
+        }
+      }, 1500);
+    });
+    return promise;
   }
 }
