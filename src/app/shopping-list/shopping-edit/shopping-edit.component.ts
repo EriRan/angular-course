114ecb279133e59bfae01783b38e20ed5b4980fa
EriRan <erik.rantanen@hotmail.fi>
2021-05-30 17:07:49 +0300

    Reset edit mode after each submit

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -43,5 +43,7 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     } else {
       this.shoppingListService.addIngredient(newIngredient);
     }
+    this.editMode = false;
+    this.shoppingListForm.reset();
   }
 }
