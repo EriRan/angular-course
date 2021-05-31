ceb0d0397a0d8b6b1165dcea6dbcc4b65d8fcf14
EriRan <erik.rantanen@hotmail.fi>
2021-05-31 19:30:47 +0300

    Add more operations for recipes
    Adding new ingredients not working properly. Its throwing the user
    out of there

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'side-project/node_modules/rxjs';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
 import { Recipe } from './recipe.model';
 
 @Injectable()
 export class RecipeService {
+  recipesChanged = new Subject<Recipe[]>();
+
   constructor(private shoppingListService: ShoppingListService) {
 
   }
@@ -35,5 +38,24 @@ export class RecipeService {
 
   addIngredientsToShoppingList(ingredients: Ingredient[]) {
     this.shoppingListService.addIngredients(ingredients);
+  }
+
+  addRecipe(recipe: Recipe) {
+    this.recipes.push(recipe);
+    this.emitUpdate();
+  }
+
+  updateRecipe(index: number, newRecipe: Recipe) {
+    this.recipes[index] = newRecipe;
+    this.emitUpdate();
+  }
+
+  deleteRecipe(index: number) {
+    this.recipes.splice(index, 1);
+    this.emitUpdate();
+  }
+
+  private emitUpdate() {
+    this.recipesChanged.next(this.recipes.slice());
   }
 }
