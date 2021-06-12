ed5e4135fed70d21c0f59c6fd2c3f62210c718c6
EriRan <erik.rantanen@hotmail.fi>
2021-06-12 13:49:40 +0300

    Add some get and post logic

--- side-project/src/app/app.component.ts
+++ side-project/src/app/app.component.ts
@@ -1,28 +1,61 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import {map} from "rxjs/operators";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent implements OnInit {
   loadedPosts = [];
 
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.fetchPosts();
+  }
 
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
-    console.log(postData);
+    //posts.json is a Firebase requirement
+    this.http
+      .post(
+        "https://angular-course-9fe36-default-rtdb.europe-west1.firebasedatabase.app/posts.json",
+        postData
+      )
+      .subscribe((response) => {
+        //No request is sent without subscribe!
+        console.log(response);
+      });
   }
 
   onFetchPosts() {
     // Send Http request
+    this.fetchPosts();
   }
 
   onClearPosts() {
     // Send Http request
+  }
+
+  private fetchPosts() {
+    //Gets have no request body
+    return this.http
+      .get(
+        "https://angular-course-9fe36-default-rtdb.europe-west1.firebasedatabase.app/posts.json"
+      )
+      .pipe(map(response => {
+        const postsArray = [];
+        for(const key in response) {
+          if (response.hasOwnProperty(key)) {
+            postsArray.push({...response[key], id: key});
+          }
+        }
+        return postsArray;
+      }))
+      .subscribe((posts) => {
+        console.log(posts);
+      });
   }
 }
