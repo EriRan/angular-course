06b76e1b770f9ada6eb6cec821184779856fe5b1
EriRan <erik.rantanen@hotmail.fi>
2021-06-20 17:15:34 +0300

    Append Auth header

--- side-project/src/app/auth-interceptor.service.ts
+++ side-project/src/app/auth-interceptor.service.ts
@@ -6,8 +6,11 @@ export class AuthInterceptorService implements HttpInterceptor {
     //This steps into a request flow: Is ran right before request leaves the application to the API
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         console.log("The request is on its way");
+        //Let's modify the request. Cannot modify the received request directly
+        const modifiedRequest = req.clone({headers: req.headers.append("Auth", "Hello")});
+
         //Now let the request go to the API
-        return next.handle(req);
+        return next.handle(modifiedRequest);
     }
 
 }
