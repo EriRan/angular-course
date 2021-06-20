0c89c57dd9dc1d63a07f19a1e12cbee974a10361
EriRan <erik.rantanen@hotmail.fi>
2021-06-20 16:34:37 +0300

    Add custom query params

--- side-project/src/app/posts.service.ts
+++ side-project/src/app/posts.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpErrorResponse, HttpHeaders } from "@angular/common/http";
+import { HttpClient, HttpErrorResponse, HttpHeaders, HttpParams } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Subject, throwError } from "rxjs";
 import { map, catchError } from "rxjs/operators";
@@ -31,11 +31,15 @@ export class PostsService {
   }
 
   fetchPosts() {
+    let searchParams = new HttpParams();
+    searchParams = searchParams.append("print", "pretty");
+    searchParams = searchParams.append("custom", "key");
     return this.http
       .get<{ [key: string]: Post }>(
         "https://angular-course-9fe36-default-rtdb.europe-west1.firebasedatabase.app/posts.json",
         {
-          headers: new HttpHeaders({"Custom-Header" : "Hello"})
+          headers: new HttpHeaders({"Custom-Header" : "Hello"}),
+          params: searchParams
         }
       )
       .pipe(
