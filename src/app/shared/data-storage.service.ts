fab880e42deba866b5d5b56848588893888eba56
EriRan <erik.rantanen@hotmail.fi>
2021-06-25 16:01:59 +0300

    Fix potential bug
    Set ingredients into an empty array of the recipe has no ingredients

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,23 +1,47 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { RecipeService } from '../recipes/recipe.service';
-import { textChangeRangeIsUnchanged } from 'typescript';
+import { map } from 'rxjs/operators';
 import { Recipe } from '../recipes/recipe.model';
 
 //This doesn't have to be added to app.module this way
 @Injectable({ providedIn: 'root' })
 export class DataStorageService {
-  constructor(private http: HttpClient, private recipesService: RecipeService) {}
+  constructor(
+    private http: HttpClient,
+    private recipesService: RecipeService
+  ) {}
 
   storeRecipes() {
     const recipes = this.recipesService.getRecipes();
     //Put here so it replaces the current content
-    return this.http.put("https://angular-course-9fe36-default-rtdb.europe-west1.firebasedatabase.app/recipes.json", recipes).subscribe((response) => {console.log(response)});
+    return this.http
+      .put(
+        'https://angular-course-9fe36-default-rtdb.europe-west1.firebasedatabase.app/recipes.json',
+        recipes
+      )
+      .subscribe((response) => {
+        console.log(response);
+      });
   }
 
   fetchRecipes() {
-    return this.http.get<Recipe[]>("https://angular-course-9fe36-default-rtdb.europe-west1.firebasedatabase.app/recipes.json").subscribe((recipes => {
-      this.recipesService.setRecipes(recipes);
-    }))
+    return this.http
+      .get<Recipe[]>(
+        'https://angular-course-9fe36-default-rtdb.europe-west1.firebasedatabase.app/recipes.json'
+      )
+      .pipe(
+        map((recipes) => {
+          return recipes.map((recipe) => {
+            return {
+              ...recipe,
+              ingredients: recipe.ingredients ? recipe.ingredients : [],
+            };
+          });
+        })
+      )
+      .subscribe((recipes) => {
+        this.recipesService.setRecipes(recipes);
+      });
   }
 }
