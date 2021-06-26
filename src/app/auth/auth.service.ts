9932c52041e7a66b8b7ad62efc1386efc6828e59
EriRan <erik.rantanen@hotmail.fi>
2021-06-26 14:10:02 +0300

    Add signup method

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'side-project/node_modules/rxjs';
+
+interface AuthResponseData {
+    idToken: string,
+    email: string,
+    refreshToken: string,
+    expiresIn: string,
+    localId: string,
+    registered: boolean
+}
+
+@Injectable({ providedIn: 'root' })
+export class AuthService {
+  constructor(private http: HttpClient) {}
+
+  signup(email: string, password: string): Observable<AuthResponseData> {
+    return this.http.post<AuthResponseData>(
+      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDxCjtBUZUEHOIaiJ_DiC5G75vDEjw6rF8',
+      { email: email, password: password, returnSecureToken: true }
+    );
+  }
+}
