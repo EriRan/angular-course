8f43edc95f0ba115c0b90159dbbe0c55522102c5
EriRan <erik.rantanen@hotmail.fi>
2021-06-26 14:17:18 +0300

    Enable signing up
    Had to fix wrong URL in auth.service due to it using the login
    URL

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -3,12 +3,12 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'side-project/node_modules/rxjs';
 
 interface AuthResponseData {
-    idToken: string,
-    email: string,
-    refreshToken: string,
-    expiresIn: string,
-    localId: string,
-    registered: boolean
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+  registered: boolean;
 }
 
 @Injectable({ providedIn: 'root' })
@@ -17,7 +17,7 @@ export class AuthService {
 
   signup(email: string, password: string): Observable<AuthResponseData> {
     return this.http.post<AuthResponseData>(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDxCjtBUZUEHOIaiJ_DiC5G75vDEjw6rF8',
+      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDxCjtBUZUEHOIaiJ_DiC5G75vDEjw6rF8',
       { email: email, password: password, returnSecureToken: true }
     );
   }
