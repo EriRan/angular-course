2e2c6a6f2a63ed6b027c6ae2220777f27e712d1a
EriRan <erik.rantanen@hotmail.fi>
2021-07-27 14:59:13 +0300

    Extract authModule
    Login still failing. Need to investigate this next

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
-  { path: 'auth', component: AuthComponent },
+  
 ];
 @NgModule({
   imports: [RouterModule.forRoot(appRoutes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,29 +5,29 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { AuthComponent } from './auth/auth.component';
 import { RecipesModule } from './recipes/recipe.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
+import { AuthModule } from './auth/auth.module';
 
 // Some kind of definition file for the component?
 // NgModule here is also a decorator
 @NgModule({
-  declarations: [AuthComponent, AppComponent, HeaderComponent],
+  declarations: [AppComponent, HeaderComponent],
   imports: [
     NgbModule,
     AppRoutingModule,
-    FormsModule,
     ReactiveFormsModule,
     BrowserModule,
     HttpClientModule,
     RecipesModule,
     ShoppingListModule,
     SharedModule,
-    CoreModule
+    CoreModule,
+    AuthModule,
   ],
   // Which component should be aware of at the app startup
   bootstrap: [AppComponent],
