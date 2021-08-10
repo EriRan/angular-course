24bad113b858311c10d1ef0c95164825de9211f6
EriRan <erik.rantanen@hotmail.fi>
2021-08-10 20:01:07 +0300

    Attempt at Typescriptifying redux
    God damn tutorial trying to point people on wrong path

--- src/app/shopping-list/store/shopping-list.actions.ts
+++ src/app/shopping-list/store/shopping-list.actions.ts
@@ -3,12 +3,8 @@ import { Ingredient } from 'src/app/shared/ingredient.model';
 
 export const ADD_INGREDIENT = 'ADD_INGREDIENT';
 
-export class AddIngredientAction implements Action {
-  readonly type = ADD_INGREDIENT;
-  payload: Ingredient;
 
-  constructor(payload: Ingredient) {
-    // Initialization inside the constructor
-    this.payload = payload;
-  }
-}
+export const addIngredient = createAction(
+  ADD_INGREDIENT,
+  props<{ Ingredient: Ingredient }>()
+);
--- src/app/shopping-list/store/shopping-list.reducer.ts
+++ src/app/shopping-list/store/shopping-list.reducer.ts
@@ -1,14 +1,15 @@
+import { createReducer, on } from '@ngrx/store';
 import { Ingredient } from '../../shared/ingredient.model';
-import { AddIngredientAction, ADD_INGREDIENT } from './shopping-list.actions';
+import { addIngredient } from './shopping-list.actions';
 
 const initialState = {
-  ingredients!: [new Ingredient('Ginger', 123), new Ingredient('Tomato', 321)],
+  ingredients: [new Ingredient('Ginger', 123), new Ingredient('Tomato', 321)],
 };
 
-// state has initialState as the default value
-export function shoppingListReducer(state = initialState, action: AddIngredientAction) {
-  switch (action.type) {
-    case ADD_INGREDIENT:
-      return { ...state, ingredients: [...state.ingredients, action.payload]}
-  }
-}
+export const shoppingListReducer = createReducer(
+  initialState,
+  on(addIngredient, (state, {Ingredient}) => ({
+    ...state,
+    Ingredient
+  }))
+);
