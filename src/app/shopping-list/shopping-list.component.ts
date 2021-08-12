f629ebd0f1d953521690b17a0550a90ed586eb75
EriRan <erik.rantanen@hotmail.fi>
2021-08-12 18:17:32 +0300

    Use redux logic in component
    No working functionality next. Maybe in next section?

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subscription } from 'side-project/node_modules/rxjs';
+import { Store } from '@ngrx/store';
+import { Observable, Subscription } from 'node_modules/rxjs';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from './shopping-list.service';
 
@@ -9,22 +10,18 @@ import { ShoppingListService } from './shopping-list.service';
   styleUrls: ['./shopping-list.component.css'],
 })
 export class ShoppingListComponent implements OnInit, OnDestroy {
-  ingredients: Array<Ingredient> = [];
-  private ingredientSubscription!: Subscription;
+  ingredients!: Observable<{ingredients: Ingredient[]}>;
 
-  constructor(private shoppingListService: ShoppingListService) {}
+  constructor(
+    private shoppingListService: ShoppingListService,
+    private store: Store<{ shoppingList: { ingredients: Ingredient[] } }>
+  ) {}
 
   ngOnInit(): void {
-    this.ingredients = this.shoppingListService.getIngredients();
-    this.ingredientSubscription = this.shoppingListService.ingredientsChanged.subscribe(
-      (ingredients: Ingredient[]) => {
-        this.ingredients = ingredients;
-      }
-    );
+    this.ingredients = this.store.select("shoppingList");
   }
 
   ngOnDestroy(): void {
-    this.ingredientSubscription.unsubscribe();
   }
 
   onEditItem(index: number) {
